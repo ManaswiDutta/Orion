7f3b8ed3911e17df6b60d0e41a405bfed559d0c0
Manaswi Dutta <manaswidutta2025@gmail.com>
2025-09-18 07:54:15 +0530

    mobile compatibility added

--- index.php
+++ index.php
@@ -10,9 +10,11 @@ $result = $conn->query("SELECT * FROM posts WHERE approved = 1 ORDER BY RAND() L
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Orion Feed</title>
   <link rel="stylesheet" href="styles.css">
-      <link rel="icon" type="image/png" href="favicon.png"/>
+  <link rel="icon" type="image/png" href="favicon.png"/>
 
 
 </head>
--- upload.php
+++ upload.php
@@ -32,6 +32,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Upload | Orion</title>
       <link rel="icon" type="image/png" href="favicon.png"/>
     <link rel="stylesheet" href="styles.css">
